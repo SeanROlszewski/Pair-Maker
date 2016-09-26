ddbaf880a068d670d58f2ad85cb9b6efcf3d3f43
SeanROlszewski <sean.r.olszewski@gmail.com>
2016-09-25 20:24:10 -0400

    playng with color

---
+++ Pair Maker/Pair Maker/UIKitExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIKitExtensions.swift
+//  Pair_Maker
+//
+//  Created by seandorian on 9/25/16.
+//  Copyright © 2016 Gosh. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var pmGreen: UIColor {
+        get {
+            return UIColor(red: 160/255, green: 178/255, blue: 137/255, alpha: 1)
+        }
+    }
+    
+    static var pmLightGreen: UIColor {
+        get {
+            return UIColor(red: 195/255, green: 204/255, blue: 184/255, alpha: 1)
+        }
+    }
+}
